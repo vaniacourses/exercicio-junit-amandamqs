ed6937bc43bcc3c4ba74d7dea1dca80df2e507cc
A. Marques <65735475+a-mqs@users.noreply.github.com>
2023-07-14 01:49:27 -0300

    Adição de novos testes

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -2,52 +2,98 @@ package calculadora;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
+
 
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
-	
-	private Calculadora calc;
-	
-	@BeforeEach
-	public void inicializa() {
-		calc = new Calculadora();
-	}
-	
-	@DisplayName("Testa a soma de dois números")
-	@Test
-	public void testSomaDoisNumeros() {
-		int soma = calc.soma(4, 5);		
-		Assertions.assertEquals(9, soma);		
-	}
-	
-	@Test
-	public void testDivisaoDoisNumeros() {
-		int divisao = calc.divisao(8, 4);
-		assertTrue(divisao == 2);
-	}
-	
-	@Test
-	public void testDivisaoPorZero() {
-		try {
-			int divisao = calc.divisao(8, 0);
-			fail("Exceção não lançada");
-		}catch (ArithmeticException e) {
-			assertEquals("/ by zero", e.getMessage());
-		}		
-	}
-	
-	@Test
-	public void testDivisaoPorZeroComAssertThrows() {
-		assertThrows(ArithmeticException.class,
-				() -> calc.divisao(8, 0));
-	}
+
+    private Calculadora calc;
+
+    @BeforeEach
+    public void inicializa() {
+        calc = new Calculadora();
+
+    }
+
+    @DisplayName("Testa a soma de dois nÃºmeros")
+    @Test
+    public void testSomaDoisNumeros() {
+        int soma = calc.soma(4, 5);
+
+        Assertions.assertEquals(9, soma);
+
+    }
+
+    @Test
+    public void testDivisaoDoisNumeros() {
+        int divisao = calc.divisao(8, 4);
+
+        assertTrue(divisao == 2);
+
+    }
+
+    public void testSubtracaoDoisNumeros() {
+        int subtracao = calc.subtracao(8, 4);
+        assertTrue(subtracao == 2);
+
+    }
+
+    public void testSMultiplicacaoDoisNumeros() {
+        int multiplicacao = calc.multiplicacao(8, 4);
+        assertTrue(multiplicacao == 2);
+
+    }
+
+
+    @Test
+    public void testDivisaoPorZero() {
+
+        try {
+            int divisao = calc.divisao(8, 0);
+            fail("ExceÃ§Ã£o nÃ£o lanÃ§ada");
+
+        } catch (ArithmeticException e) {
+            assertEquals("/ by zero", e.getMessage());
+
+        }
+
+    }
+
+
+    @Test
+    public void testDivisaoPorZeroComAssertThrows() {
+        assertThrows(ArithmeticException.class,
+                () -> calc.divisao(8, 0));
+
+    }
+
+
+    @Test
+    public void testSomaDeNumerosNegativos() {
+        int soma = calc.soma(-4, -5);
+        assertEquals(-9, soma);
+    }
+
+
+    @Test
+    public void testDivisaoDeNumerosNegativos() {
+        int divisao = calc.divisao(-8, -4);
+        assertEquals(2, divisao);
+    }
+
 
 }
